cf223c655b190b394d1d6d8a4190f8f3b9f44866
yanWAR <ali.065119057@unpak.ac.id>
2021-06-08 20:13:30 +0700

    update_db

--- database/db_sisko.sql
+++ database/db_sisko.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Jun 06, 2021 at 02:59 AM
+-- Generation Time: Jun 08, 2021 at 03:11 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.10
 
@@ -20,6 +20,57 @@ SET time_zone = "+00:00";
 --
 -- Database: `db_sisko`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kendaraan`
+--
+
+CREATE TABLE `kendaraan` (
+  `id_kendaraan` tinyint(2) NOT NULL,
+  `nomor_polisi` varchar(8) NOT NULL,
+  `merek_tipe` varchar(30) NOT NULL,
+  `kapasitas` int(2) NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  `foto` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `organisasi`
+--
+
+CREATE TABLE `organisasi` (
+  `id_organisasi` tinyint(1) NOT NULL,
+  `institusi` varchar(150) NOT NULL,
+  `nama` varchar(150) NOT NULL,
+  `status` varchar(150) NOT NULL,
+  `alamat` varchar(150) NOT NULL,
+  `website` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `logo` varchar(250) NOT NULL,
+  `id_user` tinyint(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengemudi`
+--
+
+CREATE TABLE `pengemudi` (
+  `id_pengemudi` tinyint(2) NOT NULL,
+  `nama_pengemudi` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pengemudi`
+--
+
+INSERT INTO `pengemudi` (`id_pengemudi`, `nama_pengemudi`) VALUES
+(1, 'ade');
 
 -- --------------------------------------------------------
 
@@ -46,31 +97,9 @@ CREATE TABLE `perjalanan` (
 --
 
 INSERT INTO `perjalanan` (`id_perjalanan`, `tgl_berangkat`, `tgl_pulang`, `alamat_tujuan`, `jml_penumpang`, `status`, `km_awal`, `km_akhir`, `id_user`, `id_kendaraan`, `id_pengemudi`) VALUES
-(1, '2021-06-06', '2021-06-08', 'Jakarta', 2, 0, 12000, 12560, 1, 1, 1),
-(2, '2021-06-06', '2021-06-07', 'Jawa Tengah', 5, 0, 12000, 0, 1, 1, 1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tbl_siswa`
---
-
-CREATE TABLE `tbl_siswa` (
-  `no` tinyint(3) NOT NULL,
-  `nisn` varchar(10) NOT NULL,
-  `nama_lengkap` varchar(20) NOT NULL,
-  `alamat` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `tbl_siswa`
---
-
-INSERT INTO `tbl_siswa` (`no`, `nisn`, `nama_lengkap`, `alamat`) VALUES
-(2, '065119056', 'Sri Novianti', 'Jakarta'),
-(3, '065119054', 'asli', 'korea'),
-(4, '065119050', 'ali ali', 'amerika'),
-(7, '0009', 'ali ali ali', 'bogor coret');
+(1, '2021-06-06', '2021-06-08', 'Jakarta', 2, 0, 12000, 12560, 2, 1, 1),
+(2, '2021-06-06', '2021-06-07', 'Jawa Tengah', 5, 0, 12000, 0, 3, 1, 1),
+(3, '2021-06-08', '2021-06-08', 'Leuwiliang', 2, NULL, 8000, 8125, 3, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -93,24 +122,36 @@ CREATE TABLE `tbl_user` (
 
 INSERT INTO `tbl_user` (`id_user`, `username`, `password`, `nama`, `email`, `admin`) VALUES
 (1, 'masrud', '7d05dc02abe9cda729d0c798c886db47', 'M. Rudianto', '-', 1),
-(5, 'admin', 'e10adc3949ba59abbe56e057f20f883e', 'M Ali Yanwar', '11', 2),
-(7, 'ali', '86318e52f5ed4801abe1d13d509443de', 'ali ali ali ali', '', 3);
+(2, 'admin', 'e10adc3949ba59abbe56e057f20f883e', 'ALI admin', '', 2),
+(3, 'ali', '86318e52f5ed4801abe1d13d509443de', 'ALI user', '', 3);
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `kendaraan`
+--
+ALTER TABLE `kendaraan`
+  ADD PRIMARY KEY (`id_kendaraan`);
+
+--
+-- Indexes for table `organisasi`
+--
+ALTER TABLE `organisasi`
+  ADD PRIMARY KEY (`id_organisasi`);
+
+--
+-- Indexes for table `pengemudi`
+--
+ALTER TABLE `pengemudi`
+  ADD PRIMARY KEY (`id_pengemudi`);
+
+--
 -- Indexes for table `perjalanan`
 --
 ALTER TABLE `perjalanan`
   ADD PRIMARY KEY (`id_perjalanan`);
-
---
--- Indexes for table `tbl_siswa`
---
-ALTER TABLE `tbl_siswa`
-  ADD PRIMARY KEY (`no`);
 
 --
 -- Indexes for table `tbl_user`
@@ -123,16 +164,22 @@ ALTER TABLE `tbl_user`
 --
 
 --
+-- AUTO_INCREMENT for table `kendaraan`
+--
+ALTER TABLE `kendaraan`
+  MODIFY `id_kendaraan` tinyint(2) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pengemudi`
+--
+ALTER TABLE `pengemudi`
+  MODIFY `id_pengemudi` tinyint(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `perjalanan`
 --
 ALTER TABLE `perjalanan`
-  MODIFY `id_perjalanan` tinyint(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `tbl_siswa`
---
-ALTER TABLE `tbl_siswa`
-  MODIFY `no` tinyint(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_perjalanan` tinyint(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `tbl_user`
